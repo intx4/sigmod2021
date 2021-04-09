cc38a19cca716a35b4be27f8fe05c1cff7b43387
Carlo Refice <CRefice@gmail.com>
2021-04-09 19:44:23 +0200

    Add back output generation to run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-pip install -r requirements.txt
-
 for file in X*.csv; do
 	spark-submit --packages graphframes:graphframes:0.8.1-spark3.0-s_2.12 generate_matches.py $file
 done
 
+echo "left_instance_id,right_instance_id" > output.csv
+cat *.output/*.csv >> output.csv
