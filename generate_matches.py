4f091997176b44dcb7e2e0f00db06cab5a3e8e5c
intx <francesco.intoci@epfl.ch>
2021-04-19 18:15:18 +0200

    new notebook

--- generate_matches.py
+++ generate_matches.py
@@ -26,24 +26,24 @@ df = df.select(
     "features",
 )
 
-model_1 = LinearSVCModel.load("model-" + dataset.name + "_1")
-model_comb = LogisticRegressionModel.load("model-" + dataset.name + "_comb")
-model_2 = GBTClassificationModel.load("model-" + dataset.name + "_2")
+#model_1 = LinearSVCModel.load("model-" + dataset.name + "_1")
+#model_comb = LogisticRegressionModel.load("model-" + dataset.name + "_comb")
+model_2 = GBTClassificationModel.load("model-" + dataset.name)
 
 
-output = model_1.transform(df).drop('rawPrediction').drop('probability')
-output = model_2.transform(output).drop('rawPrediction').drop('probability')
+#output = model_1.transform(df).drop('rawPrediction').drop('probability')
+output = model_2.transform(df)
 
-
+"""
 @f.udf(returnType=VectorUDT())
 def toVec(p1,p2):
     l = [p1,p2]
     return Vectors.dense(l)
+"""
 
-
-output = output.drop('features')
-output = output.withColumn('features', toVec(f.col('prediction_svc'), f.col('prediction_gbt')))
-output = model_comb.transform(output)
+#output = output.drop('features')
+#output = output.withColumn('features', toVec(f.col('prediction_svc'), f.col('prediction_gbt')))
+#output = model_comb.transform(output)
 output = output.filter(output.prediction == 1).select(
     "left_instance_id", "right_instance_id"
 )
